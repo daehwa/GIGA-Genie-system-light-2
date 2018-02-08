c2d2857cd935a51eaa454b01a672f0284974a006
daehwa <daehwakim@unist.ac.kr>
2018-02-08 11:24:50 +0900

    server

---
+++ server.js
@@ -0,0 +1,92 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var express = require('express');
+var cors =  require('cors')();
+var app = express();
+var ROOT_DIR = "html/";
+
+const ip = "13.124.195.114";
+const port = "3000";
+const PATH = "/gw/v1";
+
+app.use(cors);
+/*app.use(function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+       res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+          next();
+    });*/
+
+app.all('*', function (req, res, next) {
+  var urlObj = url.parse(req.url, true, false);
+	var dir = urlObj.pathname.split("/");
+	if(dir[1] == "sl2"){
+		fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+			if (err) {
+				console.log(err);
+				res.writeHead(404);
+				res.end(JSON.stringify(err));
+				return;
+			}
+			res.writeHead(200);
+			res.end(data);
+	  });
+	}
+	else{
+		body = "";
+		res.setHeader("Content-Type", "text/html");
+		res.writeHead(200);
+		req.on('data',function(data){
+			body += data;
+		});
+		req.on('end',function(){
+			var method = req.method;
+			var json = null;
+			if(method != 'GET')
+				json = JSON.parse(body.toString());
+			gwRequest(urlObj.pathname,method,json,returnResponse,res);
+		})
+	}
+});
+function returnResponse(res,response){
+  var serverData = '';
+  response.on('data',function(chunk){
+    serverData += chunk;
+  });
+  response.on('end',function(){
+		var d = JSON.parse(serverData);
+    res.end(serverData);
+  });
+}
+
+app.listen(20398, function () {
+  console.log('CORS-enabled web server listening on port 20398');
+});
+
+var gwRequest= function(path,m,body,callback,res){
+  var options = {
+    host: ip,
+    port: port,
+    path: PATH + path,
+    method: m,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  if(m == 'GET'){
+    http.request(options,function(response){
+      returnResponse(res,response);
+    }).end();
+  }
+  else{
+    var bodyString = JSON.stringify(body);
+    options["headers"] = {
+      'Content-Type': 'application/json',
+			'Content-Length': bodyString.length
+    };
+    http.request(options,function(response){
+			returnResponse(res,response);
+    }).write(bodyString);
+  }
+};
